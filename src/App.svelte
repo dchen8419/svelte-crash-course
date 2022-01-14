aba77e24fca59a83e5475f48e78a3373c6315b0a
David Chen <DavidChen@Davids-iMac.local>
2022-01-14 12:54:05 -0500

    added boxes and how to delete them

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,31 @@
 <script>
+	import FeedbackList from './components/FeedbackList.svelte'
+	let feedback = [
+		{
+			id: 1,
+			rating: 10,
+			text: 'Sometimes it takes a while'
+		},
+		{
+			id: 2,
+			rating: 9,
+			text: 'But you got this!'
+		},
+		{
+			id: 3,
+			rating: 5,
+			text: 'Don\'t Give up!'
+		},
+	]
 
+	const deleteFeedback = (e) => {
+		const itemId = e.detail
+		feedback = feedback.filter((item)=> item.id != itemId)
+	}
 
 </script>
 
-<main>
-	<h1>Hello World</h1>
-
+<main class="container">
+	<FeedbackList {feedback} on:delete-feedback={deleteFeedback}/>
 </main>
 
